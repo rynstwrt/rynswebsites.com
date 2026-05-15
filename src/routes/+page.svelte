dfc957159800fa0122f0653e1b2da52787899682
Ryn Stewart <rynstwrt17@gmail.com>
2026-05-14 21:18:07 -0500

    Fix layout rendering

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,25 @@
-<h1>Ryn's Websites</h1>
+<script>
+</script>
+
+<!--<h1>Ryn's Websites</h1>-->
+
+<!--<button class="btn btn-primary">Button</button>-->
+
+<div class="card w-96 bg-base-200 shadow-sm">
+    <div class="card-body">
+        <span class="badge badge-xs badge-warning">Most Popular</span>
+        <div class="flex justify-between">
+            <h2 class="text-3xl font-bold">Premium</h2>
+            <span class="text-xl font-light">$29/mo</span>
+        </div>
+        <ul class="mt-6 flex flex-col gap-2 text-xs">
+            <li>24/7 Support</li>
+            <li>24/7 Support</li>
+            <li>24/7 Support</li>
+            <li>24/7 Support</li>
+        </ul>
+        <div class="mt-6">
+            <button class="btn btn-primary btn-block">Subscribe</button>
+        </div>
+    </div>
+</div>
