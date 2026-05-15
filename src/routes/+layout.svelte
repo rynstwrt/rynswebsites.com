34f560bf90584e90aa882929c49bbee3d196f35e
Ryn Stewart <rynstwrt17@gmail.com>
2026-05-15 00:47:09 -0500

    Add preview and change bg

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
     import './layout.css';
-    import ParticlesBG from "$lib/ui/ParticlesBG.svelte";
 
     let {children} = $props();
 </script>
@@ -9,15 +8,13 @@
 <svelte:head>
     <title>Ryn's Websites</title>
     <meta name="description" content="Ryn's Websites — Web Development and Design."/>
-    <meta property="og:title" content="rynstew.art"/>
+    <meta property="og:title" content="rynswebsites.com"/>
     <meta property="og:description" content="Ryn's Websites — Web Development and Design."/>
     <meta property="og:url" content="https://rynswebsites.com"/>
-    <!--<meta property="og:image" content="https://rynstew.art/images/preview.png"/>-->
-    <!--<meta name="twitter:image" content="https://rynstew.art/images/preview.png"/>-->
-    <!--<meta name="twitter:card" content="summary_large_image"/>-->
+    <meta property="og:image" content="https://rynswebsites.com/preview.png"/>
+    <meta name="twitter:image" content="https://rynswebsites.com/preview.png"/>
+    <meta name="twitter:card" content="summary_large_image"/>
 </svelte:head>
 
-
-<ParticlesBG />
 
 {@render children()}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,41 @@
 <script lang="ts">
+    import Particles, { particlesInit } from '@tsparticles/svelte';
+    import { loadSlim } from '@tsparticles/slim';
+
+    let particlesConfig = {
+        fullScreen: {
+            // enable: true,
+            // zIndex: -1
+        },
+        detectRetina: true,
+        particles: {
+            number: {
+                value: 50,
+            },
+            links: {
+                enable: true,
+                distance: 150,
+                opacity: 0.75,
+            },
+            move: {
+                enable: true,
+                speed: 0.5,
+            }
+        },
+    }
+
+    void particlesInit(async (engine) => {
+        await loadSlim(engine);
+    });
 </script>
 
 
-<section id="landing" class="w-full h-screen flex justify-center items-center">
+<div class="w-screen h-screen fixed -z-1 opacity-10">
+    <Particles id="tsparticles" options={particlesConfig} />
+</div>
+
+
+<section id="landing" class="w-full h-screen flex justify-center items-center backdrop-blur-[1px] backdrop-brightness-85 backdrop-contrast-110 bg-transparent">
     <div class="max-w-xs">
         <h1 class="text-5xl font-bold">I'm Ryn, <br>designer and developer.</h1>
         <p class="text-lg mt-4">Your place for web development and graphic design.</p>
@@ -15,43 +48,7 @@
     </div>
 </section>
 
-<section class="bg-base-200 p-10">
+
+<section class="p-10">
     Second section
 </section>
-
-
-<!--<div class="w-full h-full flex flex-col justify-center items-center">-->
-<!--    <div>-->
-<!--        <h1 class="text-6xl font-normal uppercase leading-18 mb-8 w-full tracking-tight">Ryn's<br>Websites</h1>-->
-
-<!--        <span class="text-lg font-light bg-base-200 px-3 py-2 rounded-md">-->
-<!--            Your place for:-->
-<!--            <span class="ml-1 text-rotate duration-6000">-->
-<!--                <span class="*:bg-secondary *:text-cyan-50 *:rounded-sm *:px-2">-->
-<!--                    <span class="">Web Development 📐</span>-->
-<!--                    <span class="">Graphic Design 🎨</span>-->
-<!--                </span>-->
-<!--            </span>-->
-<!--        </span>-->
-<!--    </div>-->
-<!--</div>-->
-
-
-<!--<div class="card w-96 bg-base-200 shadow-sm">-->
-<!--    <div class="card-body">-->
-<!--        <span class="badge badge-xs badge-warning">Most Popular</span>-->
-<!--        <div class="flex justify-between">-->
-<!--            <h2 class="text-3xl font-bold">Premium</h2>-->
-<!--            <span class="text-xl font-light">$29/mo</span>-->
-<!--        </div>-->
-<!--        <ul class="mt-6 flex flex-col gap-2 text-xs">-->
-<!--            <li>24/7 Support</li>-->
-<!--            <li>24/7 Support</li>-->
-<!--            <li>24/7 Support</li>-->
-<!--            <li>24/7 Support</li>-->
-<!--        </ul>-->
-<!--        <div class="mt-6">-->
-<!--            <button class="btn btn-primary btn-block">Subscribe</button>-->
-<!--        </div>-->
-<!--    </div>-->
-<!--</div>-->
