7457e19e24201215d7d5d12783c728e3c0f07893
Ryn Stewart <rynstwrt17@gmail.com>
2026-05-14 22:20:36 -0500

    Add temp landing

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
     import './layout.css';
 
+    import { resolve } from "$app/paths";
+
     let {children} = $props();
 </script>
 
@@ -15,6 +17,13 @@
     <!--<meta name="twitter:image" content="https://rynstew.art/images/preview.png"/>-->
     <!--<meta name="twitter:card" content="summary_large_image"/>-->
 </svelte:head>
+
+
+<!--<div class="navbar absolute collapse-title bg-base-200">-->
+<!--    <div class="navbar-start">-->
+<!--        <a class="btn btn-ghost text-xl font-semibold" href={resolve("/")}>Ryn's Websites</a>-->
+<!--    </div>-->
+<!--</div>-->
 
 
 {@render children()}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,25 +1,56 @@
-<script>
+<script lang="ts">
+
 </script>
 
-<!--<h1>Ryn's Websites</h1>-->
 
+<div class="w-full h-screen flex flex-col justify-center items-center">
+        <div class="bg-base-200 max-w-md px-5 py-3 rounded-md">
+            <h1 class="text-5xl font-semibold text-primary uppercase leading-15">Ryn's<br/>Websites</h1>
+            <p class="py-5">
+                Welcome to my website!
+                I offer web design, frontend development, and graphic design services.
+            </p>
+            <button class="btn btn-primary">Learn More</button>
+        </div>
+</div>
+
+
+
+<!--<div class="w-full h-full flex flex-col justify-center items-center">-->
+<!--    <div>-->
+<!--        <h1 class="text-6xl font-normal uppercase leading-18 mb-8 w-full tracking-tight">Ryn's<br>Websites</h1>-->
+
+<!--        <span class="text-lg font-light bg-base-200 px-3 py-2 rounded-md">-->
+<!--            Your place for:-->
+<!--            <span class="ml-1 text-rotate duration-6000">-->
+<!--                <span class="*:bg-secondary *:text-cyan-50 *:rounded-sm *:px-2">-->
+<!--                    <span class="">Web Development 📐</span>-->
+<!--                    <span class="">Graphic Design 🎨</span>-->
+<!--                </span>-->
+<!--            </span>-->
+<!--        </span>-->
+<!--    </div>-->
+<!--</div>-->
+
+
+<!--<h1>Ryn's Websites</h1>-->
 <!--<button class="btn btn-primary">Button</button>-->
 
-<div class="card w-96 bg-base-200 shadow-sm">
-    <div class="card-body">
-        <span class="badge badge-xs badge-warning">Most Popular</span>
-        <div class="flex justify-between">
-            <h2 class="text-3xl font-bold">Premium</h2>
-            <span class="text-xl font-light">$29/mo</span>
-        </div>
-        <ul class="mt-6 flex flex-col gap-2 text-xs">
-            <li>24/7 Support</li>
-            <li>24/7 Support</li>
-            <li>24/7 Support</li>
-            <li>24/7 Support</li>
-        </ul>
-        <div class="mt-6">
-            <button class="btn btn-primary btn-block">Subscribe</button>
-        </div>
-    </div>
-</div>
+<!--<div class="card w-96 bg-base-200 shadow-sm">-->
+<!--    <div class="card-body">-->
+<!--        <span class="badge badge-xs badge-warning">Most Popular</span>-->
+<!--        <div class="flex justify-between">-->
+<!--            <h2 class="text-3xl font-bold">Premium</h2>-->
+<!--            <span class="text-xl font-light">$29/mo</span>-->
+<!--        </div>-->
+<!--        <ul class="mt-6 flex flex-col gap-2 text-xs">-->
+<!--            <li>24/7 Support</li>-->
+<!--            <li>24/7 Support</li>-->
+<!--            <li>24/7 Support</li>-->
+<!--            <li>24/7 Support</li>-->
+<!--        </ul>-->
+<!--        <div class="mt-6">-->
+<!--            <button class="btn btn-primary btn-block">Subscribe</button>-->
+<!--        </div>-->
+<!--    </div>-->
+<!--</div>-->
