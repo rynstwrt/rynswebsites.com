5461199b74a96a7b981d027fbe727ff628dbf901
Ryn Stewart <rynstwrt17@gmail.com>
2026-05-17 04:46:04 -0500

    Fix particle bg resize functionality

--- src/lib/components/ParticleBG.svelte
+++ src/lib/components/ParticleBG.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-    import { onMount, tick } from "svelte";
+    import { onMount } from "svelte";
     import { Particle } from "$lib/particle.ts";
 
     let canvas: HTMLCanvasElement | undefined;
@@ -11,54 +11,7 @@
     let initInnerWidth: number;
     let initInnerHeight: number;
 
-    // class Particle {
-    //     position: { x: number, y: number };
-    //     speed: number;
-    //     direction: { x: number, y: number };
-    //     color: string;
-    //     size: number;
-    //     opacity: number;
-    //
-    //     constructor(position = {x: 0, y: 0},
-    //                 speed = 1,
-    //                 direction = {x: 1, y: 0.9},
-    //                 color = "#fff000",
-    //                 size = 10,
-    //                 opacity = 1) {
-    //         this.position = position;
-    //         this.speed = speed;
-    //         this.direction = direction;
-    //         this.color = color;
-    //         this.size = size;
-    //         this.opacity = opacity;
-    //     }
-    //
-    //     update() {
-    //         if (this.position.x <= 0 || this.position.x + this.size >= innerWidth) {
-    //             this.direction.x *= -1;
-    //         }
-    //
-    //         if (this.position.y <= 0 || this.position.y + this.size >= innerHeight)
-    //             this.direction.y *= -1;
-    //
-    //         this.position.x += this.direction.x * this.speed;
-    //         this.position.y += this.direction.y * this.speed;
-    //     }
-    //
-    //     draw() {
-    //         ctx!.fillStyle = `rgba(208, 101, 68, ${this.opacity})`;
-    //
-    //         ctx?.beginPath();
-    //         ctx?.moveTo(this.position.x, this.position.y)
-    //         ctx?.rect(this.position.x, this.position.y, this.size, this.size);
-    //         // ctx?.lineTo(this.position.x, this.position.y);
-    //         ctx?.closePath();
-    //         ctx?.fill();
-    //     }
-    // }
-
     let numParticles = $derived((innerWidth + innerHeight) / 50);
-    // let linkRadius = $derived(innerWidth / 10 + innerHeight / 10);
 
     let particles: Particle[] = [];
 
@@ -68,9 +21,9 @@
             const particle = new Particle(ctx);
 
             particle.size = Math.random() * (20 - 3) + 3;
-            // particle.speed = 0.001 * (innerWidth + innerHeight) / particle.speed;
+
             const scale = (innerWidth + innerHeight) / (initInnerWidth + initInnerHeight);
-            particle.initSpeed = Math.sqrt(scale);
+            particle.initSpeed = Math.sqrt(scale) * 0.15;
             particle.speed = particle.initSpeed + Math.random() * particle.initSpeed;
 
             particle.position.x = Math.random() * innerWidth;
@@ -87,49 +40,39 @@
 
 
     onMount(() => {
-    // $effect.pre(() => {
         initInnerWidth = innerWidth;
         initInnerHeight = innerHeight;
 
-        // tick().then(() => {
-            if (!innerWidth || !innerHeight)
-                return;
+        initParticles();
 
-            initParticles();
+        function animate() {
+            requestAnimationFrame(animate);
 
-            function animate() {
-                requestAnimationFrame(animate);
+            ctx?.clearRect(0, 0, innerWidth, innerHeight);
 
-                ctx?.clearRect(0, 0, innerWidth, innerHeight);
-
-                particles.forEach(particle => {
-                    particle.update();
-                    particle.draw();
-                });
-            }
-            animate();
-        // });
+            particles.forEach(particle => {
+                particle.update();
+                particle.draw();
+            });
+        }
+        animate();
     });
 
 
 </script>
 
 
-<svelte:window
-        bind:innerWidth
-        bind:innerHeight
-        onresize={() => {
-            // const scale = (innerWidth + innerHeight) / (initInnerWidth + initInnerHeight);
-            // particles.forEach(particle => particle.speed = Math.sqrt(scale));
-            // console.log("b")
-            // initInnerWidth = innerWidth;
-            // initInnerHeight = innerHeight;
-        }}
-></svelte:window>
+<svelte:window bind:innerWidth bind:innerHeight></svelte:window>
 
 
 <canvas bind:this={canvas}
         width={innerWidth}
         height={innerHeight}
-        class="absolute top-0 left-0 bottom-0 right-0 w-screen h-screen -z-1 opacity-40 overflow-hidden">
+        class="absolute top-0 left-0 bottom-0 right-0 w-screen h-screen -z-1 opacity-40 overflow-hidden"
+        onresize={() => {
+            particles.forEach(particle => {
+                particle.position.x = Math.random() * innerWidth;
+                particle.position.y = Math.random() * innerHeight;
+            });
+        }}>
 </canvas>
