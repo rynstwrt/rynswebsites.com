c7656bf6fd0cb0854914b3344d13666d8ea0f7b8
Ryn Stewart <rynstwrt17@gmail.com>
2026-05-17 21:17:52 -0500

    Change to vercel adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,6 @@
-import adapter from '@sveltejs/adapter-static';
+// import adapter from '@sveltejs/adapter-static';
 // import adapter from '@sveltejs/adapter-auto';
+import adapter from "@sveltejs/adapter-vercel";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -7,9 +8,10 @@ const config = {
 		runes: ({ filename }) => filename.split(/[/\\]/).includes('node_modules') ? undefined : true
 	},
 	kit: {
-		adapter: adapter({
-			strict: false
-		}),
+		// adapter: adapter({
+		// 	strict: false
+		// }),
+		adapter: adapter(),
 		paths: {
 			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
 		},
